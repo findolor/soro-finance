348616a264884512fb6608ce4e88405c7d3a1256
findolor <anakisci@gmail.com>
2025-02-25 19:54:56 +0000

    update

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,12 +7,5 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 4000
-    }
   }]
 }; 
